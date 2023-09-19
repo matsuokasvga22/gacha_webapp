3f73e5dba97e9799ef43a71d722950a70ef271fc
matsuokasvga22 <matsuoka.s.vga22@gmail.com>
2023-09-19 17:02:13 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+﻿import streamlit as st
+from random import randint
+#from PIL import Image
+
+st.title("ガチャシミュレーションアプリだよ")
+st.caption("10連ガチャが引けるよ！")
+
+prob = [87, 10, 3]
+#prob = [7, 2, 1]#
+sum_prob = sum(prob)
+
+# 画像
+#image_money = Image.open("card_joker.png")
+#st.image(image, width=200)
+#image = Image.open("card_joker.png")
+#st.image(image, width=200)
+#image = Image.open("card_joker.png")
+#st.image(image, width=200)
+
+with st.form(key='gacha') :
+	# ボタン
+	gacha_btn = st.form_submit_button('ガチャを引く')
+	if gacha_btn == True:
+		st.text('ガチャを引きます')
+		for i in range(10):
+			value = randint(1, sum_prob)
+			rare = 1
+			sum_p = 0
+			for p in prob:
+			#for p in range(len(prob)):#
+				sum_p += p
+				if value <= sum_p:
+					st.text("レア度" + ("☆" * rare) + "を引いた！")
+					#prob[p] = prob - 1#
+					#sum_prob = sum(prob)#
+					break;
+				rare += 1
---
+++ app_bk.py
@@ -0,0 +1,52 @@
+﻿import streamlit as st
+from PIL import Image
+import datetime
+
+st.title("アプリだよ")
+st.caption("これはアプリだよ！")
+st.subheader("自己紹介")
+st.text("よろしくお願いします")
+code = '''
+include<stdio.h>
+int main(){
+	int x = 0;
+	printf("%d", x);
+	return 0;
+}
+'''
+st.code(code, language='c')
+
+# 画像
+image = Image.open("card_joker.png")
+st.image(image, width=200)
+
+with st.form(key='profile_form') :
+	# テキストボックス
+	name = st.text_input('名前')
+	print(name)
+
+	# セレクトボックス
+	age_category = st.selectbox("年齢層", ("子ども", "大人"))
+
+	# ラジオボタン
+	age_category = st.radio("年齢層", ("子ども", "大人"))
+	
+	# 複数選択
+	hobby = st.multiselect("趣味", ("スポーツ", "読書", "アニメ", "釣り"))
+
+	# チェックボックス
+	st.checkbox("メールマガジンを購読する")
+
+	# スライダー
+	height = st.slider('身長', min_value = 110, max_value = 210)
+
+	# 日付
+	start_date = st.date_input("開始日", datetime.date(2023, 9, 19))
+
+	# カラーピッカー
+	color = st.color_picker("テーマカラー", "#FFFFFF")
+
+	# ボタン
+	submit_btn = st.form_submit_button('送信')
+	if submit_btn == True:
+		st.text('ようこそ！' + name + "さん")
